82b85d3be00beedb18bd34e0daf5f598098d379c
godwins3 <praisegodwins4@gmail.com>
2024-02-07 22:17:15 +0300

    todays lessons

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import uvicorn
 from pydantic import BaseModel
 from typing import List
 
 app = FastAPI()
+
+templates=Jinja2Templates(directory="templates")
 
 class Student(BaseModel):
     id: str
@@ -32,7 +35,7 @@ async def student_data(name:str=Body(...), marks:int=Body(...)):
     return {"name":name, "marks": marks}
 
 @app.get('/templates')
-async def templates():
+async def my_template():
     temp = """
 <html>
 <head></head>
@@ -43,6 +46,15 @@ async def templates():
 """
     return HTMLResponse(content=temp)
 
+@app.get("/helloworld", response_class=HTMLResponse)
+async def hello(request: Request):
+    # Jinja2Templates.TemplateResponse
+    return templates.TemplateResponse(request=request, name='hello.html')
+
+@app.get("/items/{id}/", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    user_id = id
+    return templates.TemplateResponse(request=request, name="hello.html", context={"user_id": user_id})
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="127.0.0.1", port=5000, reload=True)
